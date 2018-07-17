bf52b4ded7b89cc9c107c40f0f507dd6a81121ca
osoyliz <lizosoy18@gmail.com>
2018-07-17 22:44:52 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,8 @@
-class Model
-  # Replace with Dog clas
+
+  # THIS IS WHERE THE RUBY HAPPENS (yay backend)
+  def get_fortune
+    fortunes_array = ["The last person you talked to is going to be your partner for the rest of your life", "Don't drink water, it could kill you", "you will meet batman", "Iron man will come to talk to you and take you on a date"]
+    return fortunes_array.sample
 end
+
+puts get_fortune
